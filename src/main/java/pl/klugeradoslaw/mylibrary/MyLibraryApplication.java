462caa1f14b18bf14d325dd91bf51df19641d560
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-08-20 16:25:48 +0200

    project started, added dependencies

---
+++ src/main/java/pl/klugeradoslaw/mylibrary/MyLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.mylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibraryApplication.class, args);
+    }
+
+}
