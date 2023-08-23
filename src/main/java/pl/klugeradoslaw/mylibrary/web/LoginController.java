03e0138f82644c3ae977c0dded9ea022d7603dd7
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-08-23 17:27:18 +0200

    LoginController delted becaouse formLogin works properly

--- src/main/java/pl/klugeradoslaw/mylibrary/web/LoginController.java
+++
@@ -1,9 +0,0 @@
-package pl.klugeradoslaw.mylibrary.web;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class LoginController {
-
-}
