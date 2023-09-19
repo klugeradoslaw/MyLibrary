7762fb661ea56475a981155504c153efb63027de
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-09-19 19:30:31 +0200

    only admin can add new genre

--- src/main/java/pl/klugeradoslaw/mylibrary/web/admin/GenreAdminController.java
+++ src/main/java/pl/klugeradoslaw/mylibrary/web/admin/GenreAdminController.java
@@ -1,6 +1,9 @@
 package pl.klugeradoslaw.mylibrary.web.admin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,7 @@ import java.net.URI;
 
 @Controller
 @RequestMapping("/admin")
+@Slf4j
 public class GenreAdminController {
     private final GenreService genreService;
 
@@ -21,12 +25,19 @@ public class GenreAdminController {
     }
 
     @PostMapping("/genre")
-    public ResponseEntity<?> addGenre(@RequestBody GenreDto genreDto) {
-        GenreDto addedGenre = genreService.addGenre(genreDto);
-        URI addedGenreUri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(addedGenre.getId())
-                .toUri();
-        return ResponseEntity.created(addedGenreUri).body(addedGenre);
+    public ResponseEntity<?> addGenre(@RequestBody GenreDto genreDto, Authentication authentication) {
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            GenreDto addedGenre = genreService.addGenre(genreDto);
+            URI addedGenreUri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(addedGenre.getId())
+                    .toUri();
+            log.info("Genre added to database.");
+            return ResponseEntity.created(addedGenreUri).body(addedGenre);
+        } else {
+            log.info("Access denied.");
+            return new ResponseEntity<>("error", HttpStatus.FORBIDDEN);
+        }
+
     }
 }
