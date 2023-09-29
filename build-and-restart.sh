414c6f6d2f9ea1525cbcef2a0560b6af86c6593b
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-09-29 12:04:36 +0200

    added = insted of space

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build . -t mylibrary
 docker stop mylibrary || true
 docker rm mylibrary || true
-docker run -d -p 8080:8080 --name mylibrary mylibrary
+docker run -d -p 8080:8080 --name=mylibrary mylibrary
