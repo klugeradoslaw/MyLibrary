8d1d706d8a171129cc8faf92336957ec2fc02a51
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-10-02 21:28:08 +0200

    definig .yml files (dev)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,58 +1,58 @@
--- INSERT INTO users (email, name, password)
--- VALUES
---     -- admin@admin.com / admin
---     ('admin@admin.com', 'Administrator', '{bcrypt}$2a$10$b0ZQzE.SGKoni4RaQ6Hut.7QBzQHHswK8GKNJHCxZvR2W7OcrZErG'),
---     -- kluge.radoslaw@gmail.com / pass123
---     ('kluge.radoslaw@gmail.com', 'Radosław Kluge', '{bcrypt}$2a$10$qqzCu4GlWFa.9WBLURs8YOWUR.mRKHbSsD8piMoaBqCB.VS.wKSRC'),
---     -- test@test.com / 1234
---     ('test@test.com', 'Tester','{bcrypt}$2a$10$If85EGk6daM8ZbwMjLRLweeJbVHL6mLQypWWTrWtGEmPhrtuQ9psK');
---
--- INSERT INTO user_role (name)
--- VALUES
---     ('ADMIN'),
---     ('USER');
---
--- INSERT INTO user_roles (user_id, role_id)
--- VALUES
---     (1, 1),
---     (2, 2),
---     (3, 2);
---
--- INSERT INTO  genres (name)
--- VALUES
---     ('Information Technology'),
---     ('action'),
---     ('adventure'),
---     ('comedy'),
---     ('drama'),
---     ('fantasy'),
---     ('horror'),
---     ('science fiction'),
---     ('western');
---
--- INSERT INTO books (title, author, genre_id, isbn)
--- VALUES
---     ('W pustyni i w puszczy', 'Henryk Sienkiewicz', 3, 9788324029594),
---     ('Java. Rusz głową!', 'Kathy Sierra', 1, 9788328399846),
---     ('Wydajność Javy. Szczegółowe porady dotyczące programowania i strojenia aplikacji w Javie. Wydanie II', 'Scott Oaks', 1, 9788328370319);
---
--- INSERT INTO book_rating (rating, book_id, user_id)
--- VALUES
---     (9, 1, 2),
---     (8, 2, 2),
---     (6, 2, 3);
---
--- INSERT INTO library (user_id, name)
--- VALUES
---     (1, 'My Library'),
---     (2, 'Kluge Library'),
---     (2, 'Want to buy');
---
--- INSERT INTO books_in_library(library_id, book_id)
--- VALUES
---     (1, 1),
---     (1, 2),
---     (1, 3),
---     (2, 1),
---     (2, 3),
---     (3, 2);
+INSERT INTO users (email, name, password)
+VALUES
+    -- admin@admin.com / admin
+    ('admin@admin.com', 'Administrator', '{bcrypt}$2a$10$b0ZQzE.SGKoni4RaQ6Hut.7QBzQHHswK8GKNJHCxZvR2W7OcrZErG'),
+    -- kluge.radoslaw@gmail.com / pass123
+    ('kluge.radoslaw@gmail.com', 'Radosław Kluge', '{bcrypt}$2a$10$qqzCu4GlWFa.9WBLURs8YOWUR.mRKHbSsD8piMoaBqCB.VS.wKSRC'),
+    -- test@test.com / 1234
+    ('test@test.com', 'Tester','{bcrypt}$2a$10$If85EGk6daM8ZbwMjLRLweeJbVHL6mLQypWWTrWtGEmPhrtuQ9psK');
+
+INSERT INTO user_role (name)
+VALUES
+    ('ADMIN'),
+    ('USER');
+
+INSERT INTO user_roles (user_id, role_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 2);
+
+INSERT INTO  genres (name)
+VALUES
+    ('Information Technology'),
+    ('action'),
+    ('adventure'),
+    ('comedy'),
+    ('drama'),
+    ('fantasy'),
+    ('horror'),
+    ('science fiction'),
+    ('western');
+
+INSERT INTO books (title, author, genre_id, isbn)
+VALUES
+    ('W pustyni i w puszczy', 'Henryk Sienkiewicz', 3, 9788324029594),
+    ('Java. Rusz głową!', 'Kathy Sierra', 1, 9788328399846),
+    ('Wydajność Javy. Szczegółowe porady dotyczące programowania i strojenia aplikacji w Javie. Wydanie II', 'Scott Oaks', 1, 9788328370319);
+
+INSERT INTO book_rating (rating, book_id, user_id)
+VALUES
+    (9, 1, 2),
+    (8, 2, 2),
+    (6, 2, 3);
+
+INSERT INTO library (user_id, name)
+VALUES
+    (1, 'My Library'),
+    (2, 'Kluge Library'),
+    (2, 'Want to buy');
+
+INSERT INTO books_in_library(library_id, book_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 3),
+    (3, 2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,73 +1,73 @@
--- DROP TABLE IF EXISTS  users;
--- DROP TABLE IF EXISTS  user_role;
--- DROP TABLE IF EXISTS  user_roles;
--- DROP TABLE IF EXISTS  books;
--- DROP TABLE IF EXISTS  genres;
--- DROP TABLE IF EXISTS  book_rating;
--- DROP TABLE IF EXISTS  library;
--- DROP TABLE IF EXISTS  books_in_library;
---
--- CREATE TABLE users
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     email VARCHAR(100) NOT NULL UNIQUE,
---     name VARCHAR(50) NOT NULL,
---     password VARCHAR(200) NOT NULL
--- );
---
--- CREATE TABLE user_role
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(30) NOT NULL
--- );
---
--- CREATE TABLE user_roles
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     user_id BIGINT NOT NULL,
---     role_id BIGINT NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES users(id),
---     FOREIGN KEY (role_id) REFERENCES user_role(id)
--- );
---
--- CREATE TABLE books
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     title VARCHAR(100) NOT NULL,
---     author VARCHAR(50) NOT NULL,
---     genre_id BIGINT NOT NULL,
---     isbn BIGINT UNIQUE
--- );
---
--- CREATE TABLE genres
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(50) NOT NULL UNIQUE
--- );
---
--- CREATE TABLE book_rating
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     rating INT NOT NULL,
---     book_id BIGINT NOT NULL,
---     user_id BIGINT,
---     FOREIGN  key(book_id) references books(id),
---     FOREIGN  key(user_id) references users(id)
--- );
---
--- CREATE TABLE library
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     user_id BIGINT NOT NULL,
---     name VARCHAR(50) NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES users(id)
--- );
---
--- CREATE TABLE books_in_library
--- (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     library_id BIGINT NOT NULL,
---     book_id BIGINT NOT NULL,
---     FOREIGN  key(library_id) references library(id),
---     FOREIGN  key(book_id) references books(id)
--- );
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS book_rating;
+DROP TABLE IF EXISTS library;
+DROP TABLE IF EXISTS books_in_library;
+
+CREATE TABLE users
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE user_role
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE user_roles
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES user_role(id)
+);
+
+CREATE TABLE books
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    genre_id BIGINT NOT NULL,
+    isbn BIGINT UNIQUE
+);
+
+CREATE TABLE genres
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE book_rating
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    rating INT NOT NULL,
+    book_id BIGINT NOT NULL,
+    user_id BIGINT,
+    FOREIGN  key(book_id) references books(id),
+    FOREIGN  key(user_id) references users(id)
+);
+
+CREATE TABLE library
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE books_in_library
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    library_id BIGINT NOT NULL,
+    book_id BIGINT NOT NULL,
+    FOREIGN  key(library_id) references library(id),
+    FOREIGN  key(book_id) references books(id)
+);
