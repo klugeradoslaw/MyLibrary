9051dbf2ac45e376d607561634b2823f4e6e14dd
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-11-13 17:39:57 +0100

    added JwtService (change from BasicAuthentication to Jwt)

---
+++ src/main/java/pl/klugeradoslaw/mylibrary/xconfig/JwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package pl.klugeradoslaw.mylibrary.xconfig;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
