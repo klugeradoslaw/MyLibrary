d3c5e1b581b12ff548ebc2cc28aa8cde8bb5de0b
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-11-14 11:37:20 +0100

    added JwtAuthenticationFilter and SuccesHandler

---
+++ src/main/java/pl/klugeradoslaw/mylibrary/xconfig/JwtAuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package pl.klugeradoslaw.mylibrary.xconfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.List;
+
+public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationSuccessHandler(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        List<String> authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        String signedJWT = jwtService.createSignedJWT(authentication.getName(), authorities);
+        new ObjectMapper().writeValue(response.getWriter(), new JwtWrapper(signedJWT));
+    }
+
+    private record JwtWrapper(String token){ }
+
+}
